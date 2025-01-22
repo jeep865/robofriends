ec93a05273a35a152db427b4a332ffb526b4208b
jeep865 <Jorgepoconnor@gmail.com>
2025-01-22 12:43:00 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <App />
+      <p>This is a simple React app with Tachyons CSS library</p>
+    </div>
+    
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
